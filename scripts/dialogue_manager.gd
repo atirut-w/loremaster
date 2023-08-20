2b638aab013affde6539bdf81707a4b51a0bb1a9
girdenis-p <girdenis.p@gmail.com>
2023-08-20 22:00:05 +0100

    rework dialogue_manager

--- scripts/dialogue_manager.gd
+++ scripts/dialogue_manager.gd
@@ -1,64 +1,45 @@
 extends Node
 
-signal message(dialogue, choices)
+signal start_of_dialogue
+signal message(dialogue)
 signal end_of_dialogue
 
-const STATIC_DIALOGUE = TYPE_STRING
-const CHOICE_DIALOGUE = TYPE_DICTIONARY
-const CONDITIONAL_DIALOGUE = TYPE_CALLABLE
+signal start_of_choices(options)
+signal end_of_choices
+
+const SINGLE_DIALOGUE = TYPE_STRING
 const GROUPED_DIALOGUE = TYPE_ARRAY
 
-const DIALOGUE_INFO_KEY = "info"
-const NO_CHOICE = "NO_CHOICE"
-
 var dialogues = []
+var choice = ""
 
-func progress(choice=NO_CHOICE):
+func _read_dialog():
 	if dialogues.size() > 0:
-		var dialogue = dialogues.front()
-		#For static dialogue, emit the dialogue with no choices
-		if typeof(dialogue) == STATIC_DIALOGUE:
-			dialogues.pop_front()
-			message.emit(dialogue,{})
-		#For choice dialogue, emit a header with choices if no choice is made
-		#Otherwise get dialogue from choice, replace with front and then progress
-		if typeof(dialogue) == CHOICE_DIALOGUE:
-			if choice == NO_CHOICE:
-				message.emit(dialogue[DIALOGUE_INFO_KEY], dialogue)
-			else:
-				var new_dialogue = dialogue[choice]
-				_replace_dialogue_at_front(new_dialogue)
-				progress()
-		#Execute conditional diagonal, replace with front of list and then progress
-		if typeof(dialogue) == CONDITIONAL_DIALOGUE:
-			var new_dialogue = dialogue.call()
-			_replace_dialogue_at_front(new_dialogue)
-			progress()
-		#For an empty grouped dialog, remove and then progress
-		#For grouped dialog, get first element and put to front of list and them progress
-		if typeof(dialogue) == GROUPED_DIALOGUE:
-			if dialogue.size() == 0:
-				dialogues.pop_front()
-				progress()
-			else:
-				var new_dialogue = dialogue.pop_front()
-				dialogues.push_front(new_dialogue)
-				progress()
+		var dialogue = dialogues.pop_front()
+		message.emit(dialogue)
 	else:
 		end_of_dialogue.emit()
-			
-#Add dialogue to the dialogue list, does not work for CHOICE_DIALOGUE
-func add_dialogue(dialogue):
-	if typeof(dialogue) != CHOICE_DIALOGUE:
-		dialogues.push_back(dialogue)
-	
-#Add a group of choices with a header to the dialogue list
-func add_choice_dialogue(info, dialogue):
-	if typeof(dialogue) == CHOICE_DIALOGUE:
-		dialogue[DIALOGUE_INFO_KEY] = info
-		dialogues.push_back(dialogue)
-		
-func _replace_dialogue_at_front(dialogue):
-	dialogues.pop_front()
-	dialogues.push_front(dialogue)
 
+func show_dialog(dialogue):
+	if typeof(dialogue) == SINGLE_DIALOGUE:
+		dialogues = [dialogue]
+	if typeof(dialogue) == GROUPED_DIALOGUE:
+		dialogues = dialogue
+		
+	start_of_dialogue.emit()
+	
+	await end_of_dialogue
+	
+func show_choices(dialogue, options):
+	await show_dialog(dialogue)
+	
+	choice = ""
+	start_of_choices.emit(options)
+	
+	await end_of_choices
+	
+	return choice
+	
+func _submit_choice(option):
+	choice = option
+	end_of_choices.emit()
--- temp/Text.gd
+++ temp/Text.gd
@@ -1,48 +1,101 @@
 extends Label
 
-var choiceConstructor = {}
+signal read
+signal submit_choice(choice)
+
+var dialogues = []
+var choices = []
+var reading = false
+
+var text_to_display = ""
+var timer = 0
 
 func _ready():
-	$PlayerChoice.disabled = true
+	$SubmitText.disabled = false
+	$StartDialog.disabled = true
+	$Choices.disabled = true
+	$ChooseChoice.disabled = true
+	$AddChoice.disabled = false
+	$Choices.clear()
+	
+	
+func _process(delta):
+	if reading:
+		if Input.is_action_just_pressed("dialogue_continue"):
+			if text_to_display == text:
+				read.emit()
+			else:
+				text = text_to_display
 
 
 func _on_submit_text_pressed():
 	var text_to_submit = $AddText.text
 	$AddText.clear()
+	dialogues.push_back(text_to_submit)
 	
-	if $CustomChoice.selected == 0:
-		if choiceConstructor.keys().size() == 0:
-			$DialogueManager.add_dialogue(text_to_submit)
-		else:
-			$DialogueManager.add_choice_dialogue(text_to_submit, choiceConstructor)
-			choiceConstructor = {}
+	$StartDialog.disabled = false
+	
+
+func _on_start_dialog_pressed():
+	if choices.size() != 0:
+		var choice = await $DialogueManager.show_choices(dialogues,choices)
+		text = "You have chosen " + choice
 	else:
-		var selected = str($CustomChoice.get_item_text($CustomChoice.selected))
-		if !choiceConstructor.has(selected):
-			choiceConstructor[selected] = []
-		choiceConstructor[selected].push_back(text_to_submit)
+		$DialogueManager.show_dialog(dialogues)
+	dialogues = []
+	choices = []
 
 
-func _on_progress_pressed():
-	if $PlayerChoice.disabled:
-		$DialogueManager.progress()
-	else:
-		$DialogueManager.progress(str($PlayerChoice.get_item_text($PlayerChoice.selected)))
+func _on_dialogue_manager_message(dialogue):
+	text = ""
+	text_to_display = dialogue
 	
-	if !$PlayerChoice.has_selectable_items():
-		$PlayerChoice.disabled = true
-
-
-func _on_dialogue_manager_message(dialogue, choices):
-	text = dialogue
 	
-	$PlayerChoice.clear()
-	for choice in choices.keys():
-		$PlayerChoice.disabled = false
-		if choice != "info":
-			$PlayerChoice.add_item(choice)
-			
-		
-
 func _on_dialogue_manager_end_of_dialogue():
-	text = "EOD"
+	if choices.size() == 0:
+		text = "EOD"
+		$StartDialog.disabled = true
+		$SubmitText.disabled = false
+	$TextSpeed.stop()
+	reading = false
+	
+
+func _on_dialogue_manager_start_of_dialogue():
+	$StartDialog.disabled = true
+	$SubmitText.disabled = true
+	$AddChoice.disabled = true
+	$TextSpeed.start()
+	reading = true
+
+	read.emit()
+
+
+func _on_text_speed_timeout():
+	if text.length() != text_to_display.length():
+		text += text_to_display[text.length()]
+		
+	$TextSpeed.start()
+
+
+func _on_add_choice_pressed():
+	var choice_to_submit = $AddText.text
+	$AddText.clear()
+	choices.push_back(choice_to_submit)
+	
+	$StartDialog.disabled = false
+
+
+func _on_dialogue_manager_start_of_choices(options):
+	for option in options:
+		$Choices.add_item(option)
+		
+	$Choices.disabled = false
+	$ChooseChoice.disabled = false
+	
+
+
+func _on_choose_choice_pressed():
+	var choice = $Choices.get_item_text($Choices.selected)
+	submit_choice.emit(choice)
+	
+	_ready()
