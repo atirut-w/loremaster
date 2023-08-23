992dfc09bf7193fe0c1905391806ac601a7afd4c
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-08-23 21:43:30 +0700

    Add function to hide dialogue

--- scripts/dialogue_manager.gd
+++ scripts/dialogue_manager.gd
@@ -7,15 +7,21 @@ func register_dialogue_controller(ctrl: DialogueSystem) -> void:
 		controller.queue_free()
 	controller = ctrl
 	
+	controller.visible = false
 	controller.portrait_rect.visible = false
 	controller.name_label.visible = false
 	controller.choices_container.visible = false
 
 
 func show_dialogue(dialogue: String) -> void:
+	controller.visible = true
 	controller._set_dialogue_message(dialogue)
 	controller._animate()
 	await controller.dialogue_completed
+
+
+func hide_dialogue() -> void:
+	controller.visible = false
 
 
 func set_speaker_portrait(portrait: Texture) -> void:
